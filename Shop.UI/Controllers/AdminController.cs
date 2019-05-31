9e488a14b21187581473dc155598d486afb0abd6
Denis Sidorov <dsigmat@hotmail.com>
2019-05-31 11:05:58 +0600

    Add project files.

---
+++ Shop.UI/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.ProductsAdmin;
+using Shop.Database;
+
+namespace Shop.UI.Controllers
+{
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _ctx;
+        public AdminController(ApplicationDbContext context)
+        {
+            _ctx = context;
+        }
+
+        [HttpGet("products")]
+        public IActionResult GetProducts() => Ok(new GetProducts(_ctx).Do());
+
+        [HttpGet("products/{id}")]
+        public IActionResult GetProduct(int id) => Ok(new GetProduct(_ctx).Do(id));
+
+        [HttpPost("products")]
+        public async Task<IActionResult> CreateProduct([FromBody]CreateProduct.Request request) => Ok(await new CreateProduct(_ctx).Do(request));
+
+        [HttpDelete("products/{id}")]
+        public async Task<IActionResult> DeleteProduct(int id) => Ok(await new DeleteProduct(_ctx).Do(id));
+
+        [HttpPut("products")]
+        public async Task<IActionResult> UpdateProduct([FromBody]UpdateProduct.Request request) => Ok(await new UpdateProduct(_ctx).Do(request));
+    }
+}
